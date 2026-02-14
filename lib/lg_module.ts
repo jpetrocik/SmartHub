0cea0bea0aa81a7a98efb5b7c2898bb9815056da
John Petrocik <john@petrocik.net>
2026-02-13 19:19:18 -0800

    Update lg 2018+ support

--- lib/lg_module.ts
+++ lib/lg_module.ts
@@ -5,8 +5,8 @@
  * 
  * sends system/turnOn
  **/
-import wol from 'wol'
-import LGTV from 'lgtv2';
+import { LGTV, Inputs, DefaultSettings } from 'lgtv-ip-control';
+import express from 'express';
 
 /**
  * Listening to a wildcard topic, anything after the topic is the command to control the TV
@@ -17,10 +17,13 @@ import LGTV from 'lgtv2';
  **/
 export class LgWebOSModule {
 
+
+	static readonly INPUT_LIVETV = "dtv";
+	static readonly INPUT_HDMI1 = "hdmi1";
+	static readonly INPUT_HDMI2 = "hdmi2";
+
 	config: LGConfig;
 	lgtv!: LGTV;
-	connected = false;
-	reties: 0;
 
 	constructor(config: LGConfig) {
 		this.config = config;
@@ -28,136 +31,155 @@ export class LgWebOSModule {
 		console.log("Subscribing to " + this.config.topic);
 		global.SmartHub.mqttClient.subscribe(this.config.topic + "/#");
 		global.SmartHub.mqttClient.on('message', this.onMessage.bind(this));
+
+		DefaultSettings.networkWolAddress = "192.168.1.255";
+		DefaultSettings.networkTimeout = 1000;
+
+		this.lgtv = new LGTV(
+			this.config.address,
+			this.config.macAddress,
+			this.config.key,
+			DefaultSettings
+		);
 	}
 
-	connect() {
-		this.lgtv = LGTV({
-			url: `ws://${this.config.address}:3000`,
-			timeout: 30000,
-			reconnect: 5000,
-		});
-
-		return new Promise((resolve, reject) => {
-
-			this.lgtv.on('error', (err: Error) => {
-				console.log('Connecting to TV failed!');
-			});
-
-			this.lgtv.on('connecting', () => {
-				this.reties++;
-				if (this.reties > 4) {
-					console.log("Max connection attempts exceeded, discounting");
-					this.lgtv.disconnect();
-					this.lgtv = undefined;
-					this.reties = 0;
-					return;
-				}
-				console.log('Connecting to TV...');
-			});
-
-			this.lgtv.on('connect', () => {
-				console.log('Successfully connected!');
-				this.connected = true;
-				this.reties = 0;
-				resolve(true);
-			});
-
-			this.lgtv.on('prompt', () => {
-				console.log('Please authorize on TV');
-			});
-
-			this.lgtv.on('close', () => {
-				this.connected = false;
-				console.log('Connection to TV lost');
-			});
-		});
+	async connect() {
+		let retries = 0;
+		try {
+			await this.lgtv.connect();
+			retries = 0;
+			console.log('Successfully connected!');
+			return true;
+		} catch (err) {
+			retries++;
+			if (retries > 4) {
+				console.log("Max connection attempts exceeded, disconnecting");
+				this.lgtv.disconnect();
+				retries = 0;
+				return false;
+			}
+			console.log('Connecting to TV failed!');
+			throw err;
+		}
 	}
 
-	async sendCommand(command: string, message: any, powerOn: boolean = false) {
-		console.log("Sending " + command);
-
-		if (powerOn && !this.lgtv) {
-			await this.powerOn();
+	private async sendCommand(command: any, powerOn: boolean = false) {
+		if (powerOn && !this.lgtv.connected) {
+			await this.powerOnAndConnect();
+		} else if (!this.lgtv.connected) {
+			await this.connect();
 		}
 
-		if (!this.lgtv && !this.connected) {
+		if (!this.lgtv.connected) {
 			console.log("Ignoring request, TV is not ready.");
 			return;
 		}
 
-		return new Promise((resolve, reject) => {
-			this.lgtv.request("ssap://" + command, JSON.stringify(message), (err: Error | null, res: any) => {
-				if (err) {
-					reject(err);
-					return;
-				}
-
-				resolve(res);
-			});
-		});
+		try {
+			await command();
+		} catch (err) {
+			console.log("Error sending command: " + err);
+		}
 	}
 
-	powerOn() {
-		if (this.lgtv) {
-			console.log("TV already on");
-			return;
-		}
-
-		console.log("Sending WOL to " + this.config.macAddress);
-		return new Promise(async (resolve, reject) => {
-			wol.wake(this.config.macAddress, {}, async (err: any, res: any) => {
-				if (err) {
-					reject(err);
-					return
-				}
-
-				try {
-					await this.connect();
-					console.log("TV ready...")
-					resolve(true);
-				} catch (error: any) {
-					reject(err);
-				}
-			});
-		});
+	async powerOnAndConnect() {
+		await this.lgtv.powerOn();
+		await this.lgtv.connect({ maxRetries: 25, retryTimeout: 5000 });
 	}
 
 	async powerOff() {
-		await this.sendCommand("system/turnOff", {}, true);
-		this.lgtv.disconnect();
-		this.lgtv = undefined;
+		await this.lgtv.disconnect();
+		await this.lgtv.powerOff();
 	}
 
-	volumeUp() {
-		return this.sendCommand("audio/volumeUp", {});
+	async volumeUp() {
+		await this.sendCommand(async () => {
+			let currentVolume = await this.lgtv.getCurrentVolume() + 1;
+			await this.lgtv.setVolume();
+		});
 	}
 
-	volumeDown() {
-		return this.sendCommand("audio/volumeDown", {});
+	async volumeDown() {
+		await this.sendCommand(async () => {
+			let currentVolume = await this.lgtv.getCurrentVolume() - 1;
+			await this.lgtv.setVolume(currentVolume);
+		});
 	}
 
-	volume(level: any) {
-		return this.sendCommand("audio/setVolume", { volume: level });
+	async volume(level: any) {
+		await this.sendCommand(async () => {
+			await this.lgtv.setVolume(level);
+		});
 	}
 
-	channel(channelNum: any) {
-		return this.sendCommand("tv/openChannel", { channelNumber: channelNum }, true);
+	async channel(channelNum: any) {
+		await this.sendCommand(async () => {
+			await this.lgtv.setDigitalChannel(channelNum);
+		}, true);
 	}
 
-	/**
-	 * com.webos.app.hdmi1
-	 * com.webos.app.livetv
-	 **/
-	input(input: any) {
-		return this.sendCommand("system.launcher/launch", { id: input }, true);
-	}
-
-	onMessage(topic: any, message: any) {
-		if (topic.startsWith(this.config.topic)) {
-			console.log("Message recieved on " + topic);
-			var command = topic.replace(this.config.topic + "/", "");
-			this.sendCommand(command, JSON.parse(message));
+	async input(input: any) {
+		if (Inputs[input]) {
+			await this.sendCommand(async () => {
+				await this.lgtv.setInput(Inputs[input]);
+			}, true);
+		} else {
+			console.log("Unknown input: " + input);
 		}
+	}
+
+	//{topic_prefix}/audio/setVolume with payload { "volume": 15 }
+	onMessage(topic: any, payload: any) {
+		if (topic.startsWith(this.config.topic)) {
+			console.log("Message received on " + topic);
+			const command = topic.replace(this.config.topic + "/", "");
+
+			const message = payload.toString();
+			switch (command) {
+				case "audio/volumeUp":
+					return this.volumeUp();
+				case "audio/volumeDown":
+					return this.volumeDown();
+				case "audio/setVolume":
+					return this.volume(message);
+				case "system/turnOff":
+					return this.lgtv.powerOff();
+				case "tv/openChannel":
+					return this.channel(message);
+				case "system.launcher/launch":
+					return this.input(Inputs[message]);
+				default:
+					console.log("Unknown command: " + command);
+					return;
+			}
+		}
+	}
+
+	static registerApis() {
+		let router = express.Router();
+
+		console.log("Registering /watchTV")
+		router.post("/watchTV/:id", async (req, res) => {
+
+			let chanNum = req.query.chanNum;
+
+			try {
+				//Switch TV input to LiveTV
+				await global.SmartHub.modules[req.params.id].input(LgWebOSModule.INPUT_LIVETV);
+
+				//Change change
+				await global.SmartHub.modules[req.params.id].channel(chanNum);
+
+				console.log("watchTV completed");
+				res.sendStatus(200);
+			} catch (err) {
+				console.log(err);
+
+				res.sendStatus(500);
+			}
+		});
+
+		global.SmartHub.express.use('/api', router);
 	}
 
 	static init() {
@@ -169,6 +191,8 @@ export class LgWebOSModule {
 		(global.SmartHub.config.lg as LGConfig[]).forEach((config) => {
 			global.SmartHub.modules[config.id] = new LgWebOSModule(config);
 		});
+
+		LgWebOSModule.registerApis();
 	}
 }
 
@@ -177,4 +201,5 @@ interface LGConfig {
 	topic: string;
 	address: string;
 	macAddress: string;
+	key: string; // lgtv-ip-control pairing key
 }
